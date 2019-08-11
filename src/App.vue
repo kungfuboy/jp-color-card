6321cd03e45e56fa5168f23cfd3fe32eb70373d6
dark <dark@dark.local>
2019-08-11 19:50:05 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,47 @@
 <template>
   <div id="app">
     <section class="main" :style="{backgroundColor: getColor.color, color: getColor.text}">
-      <ul class="color-list">
-        <li
-          v-for="(item, index) in colorList"
-          :key="index"
-          :style="{borderColor: item.color}"
-          @click="handleClick(index)"
-        >{{item.label}}</li>
-      </ul>
-      <div class="broad" >
+      <section class="content">
+        <ul class="color-list">
+          <li
+            v-for="(item, index) in colorList"
+            :key="index"
+            :style="{borderColor: item.color}"
+            @click="handleClick(index)"
+          >{{item.label}}</li>
+        </ul>
+      </section>
+      <div class="broad">
         <h1>{{getColor.label}}</h1>
         <h3>{{getColor.jp}}</h3>
         <h2 @click="handleCopy(getColor.color)">{{getColor.color}}</h2>
         <h2 @click="handleCopy(`rgb(${getColor.rgb})`)">RGB: {{getColor.rgb}}</h2>
       </div>
+      <transition name="fade">
+        <span class="tips" v-if="isShowTip">已复制</span>
+      </transition>
       <textarea id="input"></textarea>
     </section>
   </div>
 </template>
 <script>
 import color from "@/assets/color";
+import { setTimeout } from "timers";
 
-function randomNum (minNum, maxNum) { 
+function randomNum(minNum, maxNum) {
   // 生成随机数
-    switch(arguments.length){ 
-        case 1: 
-            return parseInt(Math.random()*minNum+1,10); 
-        break; 
-        case 2: 
-            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
-        break; 
-            default: 
-                return 0; 
-            break; 
-    } 
-} 
+  switch (arguments.length) {
+    case 1:
+      return parseInt(Math.random() * minNum + 1, 10);
+      break;
+    case 2:
+      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+      break;
+    default:
+      return 0;
+      break;
+  }
+}
 const countColor = value => {
   var _value = "0x" + value.replace(/#/g, "");
   var str = "000000" + (0xffffff - _value).toString(16);
@@ -65,10 +71,10 @@ const countRgb = value => {
 };
 
 export default {
-  data: () => ({ colorList: [], activeIndex: 0 }),
+  data: () => ({ colorList: [], activeIndex: 0, isShowTip: false }),
   beforeMount() {
     this.colorList = color;
-    this.activeIndex = randomNum(0, color.length - 1)
+    this.activeIndex = randomNum(0, color.length - 1);
   },
   methods: {
     handleClick(index) {
@@ -79,6 +85,10 @@ export default {
       input.value = color; // 修改文本框的内容
       input.select(); // 选中文本
       document.execCommand("copy"); // 执行浏览器复制命令
+      this.isShowTip = true;
+      setTimeout(() => {
+        this.isShowTip = false;
+      }, 1000);
     }
   },
   computed: {
@@ -117,11 +127,19 @@ body {
 }
 .main {
   height: 100%;
+  width: 100%;
   background-image: url("./assets/texture.png");
   transition: 1s all ease;
   overflow: auto;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  .content {
+    max-width: 1200px;
+  }
   .color-list {
-    width: 800px;
+    min-width: 500px;
+    max-width: 800px;
     display: flex;
     flex-wrap: wrap;
     li {
@@ -140,8 +158,9 @@ body {
     position: fixed;
     text-align: right;
     font-weight: 100;
-    right: 60px;
-    top: 0;
+    left: calc(50vw + 400px);
+    top: 50px;
+    width: 200px;
     h1 {
       width: 1em;
       font-weight: normal;
@@ -154,6 +173,33 @@ body {
       cursor: pointer;
     }
   }
-  #input {position: absolute;top: 0;left: 0;opacity: 0;z-index: -10;}
+  .tips {
+    width: 80px;
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    left: calc(50vw + 600px - 80px);
+    top: 64px;
+    background: rgba(255, 255, 255, 0.5);
+    border-radius: 4px;
+    color: #2c3e50;
+    box-shadow: 0 0 8px rgba(255, 255, 255, 0.8);
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+  #input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    z-index: -10;
+  }
 }
 </style>
